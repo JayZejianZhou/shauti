4f675deaed70940c64eb5905b42c35f7231ba02e
JayZejianZhou <JayZejianZhou@users.noreply.github.com>
2020-10-29 13:34:56 -0700

    LC658教程解法

--- LC658.cpp
+++ LC658.cpp
@@ -1,74 +1,55 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int mid = 0;
-        int start = 0;
-        int end = arr.size();
         vector<int> res;
-        
-        //sort the mother fucker
-        sort(arr.begin(), arr.end());
-        
-        //binary search
-        while(start+1 < end){
-            mid = start + (end - start)/2;
-            if(arr[mid] < x){
-                start = mid;
-            }
-            else if(arr[mid] > x){
-                end = mid;
-            }
-            else{
-                start = end = mid;
-            }
-        }
-        
-        //循环insert
-        int indMin=0;
-        bool flagStart=false; //start hit the begininng
-        bool flagEnd=false; //end hit the end
-
-        
+        int right= binary_search(arr, x);
+        int left = right-1;
         do{
-            indMin = argmin(abs(arr[start]-x), abs(arr[end]-x), start, end);
-            if(indMin==start && !flagStart){
-                res.push_back(arr[start]);
-                if(start>0){
-                    start--;
-                }
-                else{
-                   flagStart = true; 
-                }
-                
-            }
-            else if(indMin == end && !flagEnd){
-                res.push_back(arr[end]);
-                if (end >= arr.size() -1){
-                    flagEnd = true;                    
-                }
-                else{
-                    end++;
-                }
-                
+            if(is_left_closer(arr, left, right, x)){
+                res.push_back(arr[left]);
+                left --;
             }
             else{
-                res[0]=-1;
-                return res;
+                res.push_back(arr[right]);
+                right++;
             }
-
-        }while (res.size()<k);
-        
-        sort(res.begin(), res.end());
+        }while(res.size()<k);
+        sort(res.begin(),res.end());
         return res;
     }
     
-    private:
-       int argmin(int a, int b, int indA, int indB){
-           if(a>b){
-               return indB;
-           }
-           else{
-               return indA;
-           }
-     } 
+private:
+    int binary_search(vector<int>& arr, int target){
+        int start = 0;
+        int mid=0;
+        int end = arr.size()-1;
+        while (start+1 < end){
+            mid = start + (end - start)/2;
+            if(arr[mid]>target){
+                end = mid; 
+            }
+            else if(arr[mid]<target){
+                start = mid;
+            }
+            else if (arr[mid]==target){
+                start = end = mid;
+            }
+            else{
+                return -1;
+            }
+        }
+        return end;
+    }
+    
+    bool is_left_closer(vector<int>& arr, int left, int right, int target){
+        if(left<0){
+            return false;
+        }
+        else if (right>=arr.size()){
+            return true;
+        }
+        else{
+            return abs(arr[left]-target)<= abs(arr[right]-target);
+        }
+    }
 };
